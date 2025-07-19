747ad1f3df4df193a4731ce7de901a8f71f598d5
Chetan Krishna Palicherla <52138415+ChetanKrishna07@users.noreply.github.com>
2025-07-19 18:53:45 -0400

    Cleaned up for pr

--- quiz_app/src/utils/ai.jsx
+++ quiz_app/src/utils/ai.jsx
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000"
-// const API_BASE_URL = "http://localhost:4000"
-console.log("API_BASE_URL from ai.jsx: ", API_BASE_URL);
-
 
 const extractTopics = async (textContent, currentTopics = []) => {
   try {
--- quiz_app/src/utils/api.js
+++ quiz_app/src/utils/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
-// const API_BASE_URL = "http://localhost:4000";
-console.log("API_BASE_URL from api.js: ", API_BASE_URL);
 
 // API utility functions for user and document management
 
