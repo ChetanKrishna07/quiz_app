833c1d4c01d9b2d12c62e8a87f7b48076d988cf6
Chetan Krishna Palicherla <52138415+ChetanKrishna07@users.noreply.github.com>
2025-07-19 23:18:25 -0400

    actions new token test

--- api/main.py
+++ api/main.py
@@ -54,7 +54,7 @@ class GenerateDocumentNameRequest(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World v3 rearanged with login with buildx and push!"}
+    return {"message": "Hello, World v3 new token!"}
 
 @app.post("/parse_file")
 async def parse_file(file: UploadFile = File(...)):
