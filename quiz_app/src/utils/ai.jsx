3ebdeabfc34fd577e233a1cca12cfaa7595d7397
Chetan Krishna Palicherla <52138415+ChetanKrishna07@users.noreply.github.com>
2025-08-02 18:46:09 -0400

    Reset API URL

--- quiz_app/src/utils/ai.jsx
+++ quiz_app/src/utils/ai.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { getUserScores } from "./api";
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000"
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000"
+// const API_BASE_URL = "http://localhost:8000";
 
 const extractTopics = async (textContent, currentTopics = []) => {
   try {
